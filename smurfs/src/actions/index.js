f75b0cfb3cc10f3fa073db367530f5788be9dfd9
myke47 <mychel.larson@gmail.com>
2019-07-01 13:22:58 -0700

    Added dependencies and dev server, Added Actions

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -3,6 +3,10 @@
   Be sure to export each action type so you can pull it into your reducer
 */
 
+import axios from 'axios';
+
+
+
 /*
   For this project you'll need at least 2 action creators for the main portion,
    and 2 more for the stretch problem.
@@ -13,3 +17,26 @@
    U - updateSmurf
    D - deleteSmurf
 */
+
+export const getSmurfs = () => dispatch => {
+  dispatch({ type: FETCHING_SMURFS });
+  axios
+  .get('http://localhost:3333/smurfs')
+  .then(res => dispatch({ type: ADDING_SMURFS_SUCCESSFUL, payload: res.data }))
+  .catch(err => dispatch({ type: FETCHING_SMURFS_FAILED, payload: err }))
+};
+
+export const addSmurf = smurf => dispatch => {
+  dispatch({ type: ADDING_SMURF_START });
+  axios
+  .post('http://localhost:3333/smurfs', smurf)
+  .then(res => dispatch({ type: ADDING_SMURF_SUCCESSFUL, payload: res.data }))
+};
+
+export const deleteSmurf = id => dispatch => {
+  dispatch({ type: DELETING_SMURF_START })
+  axios
+  .delete(`http://localhost:3333/smurfs/${id}`)
+  .then(res => dispatch({ type: DELETING_SMURF_SUCCESSFUL, payload: res.data }))
+  .catch(err => dispatch({ type: DELETING_SMURF_FAILED, payload: err }))
+};
